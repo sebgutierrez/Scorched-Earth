9b3a017d8f4916456cd3f9d9c3e5132555841c1e
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-12-20 20:44:32 -0600

    Implemented Nav Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation';
 
 function App() {
 
   const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    fetch('http://localhost/welcome') /* change fetch url to the url the flask server is running on + /welcome */
+    fetch('http://localhost:5000/welcome') /* change fetch url to the url the flask server is running on + /welcome */
     .then(response => response.json())
     .then(data => {
           setData(data)
@@ -17,11 +18,12 @@ function App() {
 
   return (
     <div>
+        <Navigation></Navigation>
 
-        { (typeof data === 'undefined') 
+        {/* (typeof data === 'undefined') 
         ? ( <h1>Loading...</h1> ) 
         : ( <h1>{data.message}</h1> ) 
-        } 
+        */} 
       
     </div>
   );
