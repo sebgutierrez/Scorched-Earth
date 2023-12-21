65317fe1a27cead53c84260416389ee215fa74ca
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-12-20 18:23:10 -0600

    Map can load now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+"use client";
+import { Fragment, useState, useEffect } from 'react';
+
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  InfoWindowF
+} from "@react-google-maps/api"
+
+import './App.css';
+
+const markers = [
+  {
+    id: 1,
+    name: 'Houston',
+    position: { lat: 29.749907, lng: -95.358421 }
+  }
+]
 
 function App() {
 
   const [data, setData] = useState([{}]);
 
+  /*const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_API_KEY
+  })*/
+
   useEffect(() => {
-    fetch('http://localhost/welcome') /* change fetch url to the url the flask server is running on + /welcome */
+    fetch('http://127.0.0.1:5000/welcome') /* change fetch url to the url the flask server is running on + /welcome */
     .then(response => response.json())
     .then(data => {
           setData(data)
@@ -16,14 +37,32 @@ function App() {
     }, []);
 
   return (
-    <div>
+    <Fragment> 
 
-        { (typeof data === 'undefined') 
-        ? ( <h1>Loading...</h1> ) 
-        : ( <h1>{data.message}</h1> ) 
-        } 
+        <div className="container">
+          { (typeof data === 'undefined') 
+          ? ( <h1>Loading...</h1> ) 
+          : ( <h1>{data.message}</h1> ) 
+          } 
+        </div>
       
-    </div>
+        <div className="map-container">
+          {
+            /*
+            isLoaded ? (
+            <GoogleMap
+              center={{ lat: 29.749907, lng: -95.358421 }}
+              zoom={7}
+              mapContainerStyle={{
+                width: "100%",
+                height: "100vh",
+              }}>
+            </GoogleMap>
+            ) : null */
+          }
+        </div>
+
+    </Fragment>
   );
 }
 
