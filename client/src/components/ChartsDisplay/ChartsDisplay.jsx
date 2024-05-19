4e267d520cfb20859e15d4aefe8b84f4a5e2b338
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2024-05-19 01:18:14 -0500

    added fahrenheit to celsius function to charts and forecast containers and tweaked some styling

--- client/src/components/ChartsDisplay/ChartsDisplay.jsx
+++ client/src/components/ChartsDisplay/ChartsDisplay.jsx
@@ -1,180 +1,218 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Chart from "react-apexcharts";
+import './ChartsDisplay.css';
 
 const ChartDisplay = (props) => {
 	const [options, setOptions] = useState({
 		options: {
 			chart: {
-			  id: "line",
-			  type: "line",
-			  toolbar: {
-				show: false,
-				tools: {
-					download: false,
-					selection: false,
-					zoom: false,
-					zoomin: false,
-					zoomout: false,
-					pan: false,
-					reset: false
+				id: "line",
+				type: "line",
+				toolbar: {
+					show: false,
+					tools: {
+						download: false,
+						selection: false,
+						zoom: false,
+						zoomin: false,
+						zoomout: false,
+						pan: false,
+						reset: false
+					}
+				},
+				zoom: false,
+				animations: {
+					enabled: false,
+					dynamicAnimation: {
+						enabled: true,
+						speed: 350
+					},
 				}
-			  },
-			  zoom: false,
-			  animations: {
-				enabled: false,
-				// dynamicAnimation: {
-				// 	enabled: true,
-				// 	speed: 350
-				// }
-			}
+			},
+			legend: {
+				position: 'top',
+				offsetX: 30,
+				offsetY: 16
 			},
 			xaxis: {
 				type: "datetime",
 				tickPlacement: 'on',
-				min: '12/23/2023',
+				min: new Date("12/24/2023").getTime() - 6*3.6e+6,
+				max: new Date("01/05/2024").getTime() + 0,
 				labels: {
-					offsetX: -36.5,
+					offsetX: 12,
          			offsetY: 0,
 					datetimeFormatter: {
 						year: 'yyyy',
-						month: "dd MMM",
-						day: 'dd MMM',
+						month: "MMM dd",
+						day: 'MMM dd',
 					},
 				},
 				axisTicks: {
-					offsetX: -36.5,
+					offsetX: 12, // -36.5
          			offsetY: 0,
 				},
 			},
 			yaxis: {
-				title: {
-					text: "Avg. Temperature (°F)",
-					style: {
-					  color: "#000000",
-					  fontWeight: '500'
-					}
-				},
-				labels: {
-					formatter: (value) => { return `${value}°F` },
-				}
+					title: {
+						text: "Avg. Temperature (°F)",
+						style: {
+						color: "#000000",
+						fontWeight: '500'
+						}
+					},
+					labels: {
+						formatter: (value) => { return `${value}°F` }
+						},
 			},
 			tooltip: {
 				enabled: true,
+				shared: true,
 				followCursor: false,
+				x: {
+					format: 'MMM dd'
+				},
 				fixed: {
 					enabled: true,
-					offsetX: 160,
-					offsetY: 30,
+					offsetX: 170,
+					offsetY: 0,
 				}
+				// custom: (props) => {
+				// 	const seriesIndex = 0;
+				// 	const left = props.w.globals.seriesXvalues[seriesIndex][props.dataPointIndex];
+				// 	const top = props.w.globals.seriesYvalues[seriesIndex][props.dataPointIndex];
+				// 	const div = props.w.globals
+				// 	return `<div style="left: ${left}px; top: ${top}px;" className=" bg-white rounded-md shadow-[0px_2px_7px_rgba(0,0,0,0.1)] p-2 absolute transform translate-x-[50%] translate-y-[-50%]">Content</div>`;
+				//   },
 			}
 		}
 	});
 	const [series, setSeries] = useState({
 		series: [
 			{
-			  name: "OpenWeather",
-			  data: [
-				{
-					x: '12/24/2023', 
-					y: 0.9
-				}, 
-				{
-					x: '12/25/2023', 
-					y: -0.1
-				},
-				{
-					x: '12/26/2023', 
-					y: -3.5
-				}, 
-				{
-					x: '12/27/2023', 
-					y: 4.3
-				}, 
-				{
-					x: '12/28/2023', 
-					y: 7.2
-				},
-				{
-					x: '12/29/2023', 
-					y: 10.9
-				},
-				{
-					x: '12/30/2023', 
-					y: 4.7
-				},
-				{
-					x: '12/31/2023', 
-					y: 6.2
-				},
-				{
-					x: '01/01/2024', 
-					y: 4.1
-				}, 
-				{
-					x: '01/02/2024', 
-					y: 2.5
-				}, 
-				{
-					x: '01/03/2024', 
-					y: 7.4
-				},
-				{
-					x: '01/04/2024', 
-					y: 4.1
-				},
-				{
-					x: '01/05/2024', 
-					y: 5.0
-				}
-			  ]
+			  name: "OpenWeather Model",
+			  data: props.openWeatherData.map((data, key) => {
+						return ({
+							x: data[0],
+							y: data[1]
+						});
+					})
 			},
 			{
-			  name: "Model Forecast",
-			  data: [
-				{
-					x: '12/30/2023', 
-					y: 4.7
-				},
-				{
-					x: '12/31/2023', 
-					y: 3.9
-				},
-				{
-					x: '01/01/2024', 
-					y: 2.7
-				}, 
-				{
-					x: '01/02/2024', 
-					y: 5.2
-				}, 
-				{
-					x: '01/03/2024', 
-					y: 9.4
-				},
-				{
-					x: '01/04/2024', 
-					y: 10.4
-				},
-				{
-					x: '01/05/2024', 
-					y: 9.7
-				}
-			  ]
+			  name: "Model",
+			  data: props.modelData.map((data, key) => {
+						return ({
+							x: data[0],
+							y: data[1]
+						});
+			  		})
 			},
 		  ]
 	});
-	  
+	const celsius = useRef(false);
+	function toCelsius(){
+		celsius.current = !celsius.current;
+		setOptions({
+			options: {
+			...options.options,
+				xaxis: {
+					...options.options.xaxis,
+					labels: {
+						offsetX: 12,
+						offsetY: 0,
+						datetimeFormatter: {
+							year: 'yyyy',
+							month: "MMM dd",
+							day: 'MMM dd',
+						},
+					},
+					axisTicks: {
+						offsetX: 12, // -36.5
+						offsetY: 0,
+					},
+				},
+				yaxis: {
+					title: {
+						...options.options.yaxis.title,
+						text: celsius.current ? "Avg. Temperature (°C)" : "Avg. Temperature (°F)",
+					},
+					labels: {
+						formatter: (value) => { 
+							if (celsius.current){
+								return `${value}°C`;
+							}
+							else{
+								return `${value}°F`;
+							}
+						},
+					}
+				},
+				tooltip: {
+					...options.options.yaxis.tooltip,
+					fixed: {
+						enabled: true,
+						offsetX: 0,
+						offsetY: 0,
+					}
+				}
+			}
+		});
+		setSeries({
+			series: [
+				{
+				  name: "OpenWeather Model",
+				  data: props.openWeatherData.map((data, key) => {
+							if(celsius.current){
+								let temp = (data[1] - 32) * 5/9;
+								return ({
+									x: data[0],
+									y: temp.toFixed(1)
+								});
+							}
+							else {
+								return ({
+									x: data[0],
+									y: data[1]
+								});
+							}
+						})
+				},
+				{
+				  name: "Model",
+				  data: props.modelData.map((data, key) => {
+							if(celsius.current){
+								let temp = (data[1] - 32) * 5/9;
+								return ({
+									x: data[0],
+									y: temp.toFixed(1)
+								});
+							}
+							else {
+								return ({
+									x: data[0],
+									y: data[1]
+								});
+							}
+				  })
+				},
+			  ]
+		});
+	}
 	return (
 		<div className="flex w-[80vw]">
-			<div className="flex w-full">
-				<div className="flex text-black border-b-2 border-l-2 border-r-2 border-slate-100">
+			<div className="flex items-middle w-full">
+				<div className="flex relative h-full items-center text-black border-slate-100">
 					<Chart
-					options={options.options}
-					series={series.series}
-					type="line"
-					width="650px"
-					height="259px"
+						className="border-2 pr-4 h-fit rounded-md"
+						options={options.options}
+						series={series.series}
+						type="line"
+						width="640px"
+						height="250px"
 					/>
+					<div className="flex absolute h-fit top-5 right-2" onClick={toCelsius}>
+						{ celsius.current ? <button className="text-black bg-white border-2 px-2 py-1 rounded-md">&deg;F</button> : <button className="text-black bg-white border-2 px-2 py-1 rounded-md">&deg;C</button> }
+					</div>
 				</div>
         	</div>
 		</div>
--- client/src/components/ForecastDisplay/ForecastDisplay.jsx
+++ client/src/components/ForecastDisplay/ForecastDisplay.jsx
@@ -1,50 +1,28 @@
-import React, { setState } from 'react';
+import React, { useState } from 'react';
 
+import TempCard from '../TempCard/TempCard';
 const ForecastDisplay = (props) => {
 	let expected = props.expected;
 	let predicted = props.predicted;
-
-
+	const [celsius, setCelsius] = useState(false);
+	function toCelsius(){
+		setCelsius((celsius) => !celsius);
+	}
 	return ( 
 	<div className="flex flex-col w-[80vw] overflow-y-auto">
 		<div className="flex w-full justify-between pt-2">
-			<div className="flex gap-x-5">
-				<button className="text-black font-bold text-xl active:text-[#145DA0]">LAST WEEK</button>
-				<button className="text-slate-500 font-bold text-xl">5-DAY FORECAST</button>
+			<div className="flex justify-between w-full">
+				<div className='flex gap-x-5'>
+					<button className="text-black font-bold text-xl active:text-[#145DA0]">LAST WEEK</button>
+					<button className="text-slate-500 font-bold text-xl">5-DAY FORECAST</button>
+				</div>
+				<div className="flex pr-1" onClick={toCelsius}>
+					{ celsius ? <button className="text-black bg-white border-2 px-2.5 py-1 rounded-md">&deg;F</button> : <button className="text-black bg-white border-2 px-2.5 py-1 rounded-md">&deg;C</button> }
+				</div>
 			</div>
 		</div>
-		{/* <p className="text-black mt-12 text-left">Last week's daily average temperatures for the city of Austin, TX, USA. Data collected using the <a className="text-[#22ccff]" href="https://openweathermap.org/city/4699066" target="_blank">OpenWeather API</a>.</p> */}
-		<ol className="grid grid-flow-col justify-stretch gap-x-2 mt-2 w-full overflow-x-auto">
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Dec 24</span>
-			<span className="text-white font-bold text-2xl">0.9&deg;F</span>
-			</li>
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Dec 25</span>
-			<span className="text-white font-bold text-2xl">-0.1&deg;F</span>
-			</li>
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Dec 26</span>
-			<span className="text-white font-bold text-2xl">-3.5&deg;F</span>
-			</li>
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Dec 27</span>
-			<span className="text-white font-bold text-2xl">4.3&deg;F</span>
-			</li>
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Dec 28</span>
-			<span className="text-white font-bold text-2xl">7.2&deg;F</span>
-			</li>
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Dec 29</span>
-			<span className="text-white font-bold text-2xl">10.9&deg;F</span>
-			</li>
-			<li className="flex flex-col justify-center items-center relative px-10 py-10 rounded-md bg-[#2C74FF]">
-			<span className="text-white absolute top-2 left-2 text-left">Today</span>
-			<span className="text-white font-bold text-2xl">4.7&deg;F</span>
-			</li>
-		</ol>
-	<p className="text-black italic text-left pt-2"><i>Last Updated: 5/15/2024</i></p>
+		<TempCard celsius={celsius} openWeatherData={props.openWeatherData}></TempCard>
+	{/* <p className="text-black italic text-left pt-2"><i>Last Updated: 5/15/2024</i></p> */}
 	</div>
   );
 }
