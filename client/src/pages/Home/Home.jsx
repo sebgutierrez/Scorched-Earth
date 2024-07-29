009fe6d1918de54734e7a5f78ce42b16132f13c8
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2024-07-28 20:41:08 -0500

    made home page responsive

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,25 +6,23 @@ import { Link } from "react-router-dom";
 
 function Home(){
     return (
-        <Fragment>
-            <div className="cover-content">
-                <div className="team-name-container">
-                    <h1 className="team-name">Weather Wizards</h1>
-                </div>
-                <div className="project-content">
-                    <h2>Scorched Earth</h2>
-                    <img src="./src/assets/cloud.svg" alt="cloud" />
-                </div>
-                <div className="project-nav">
-                    <ul>
-                        <li><Link to="/">HOME</Link></li>
-                        <li><Link to="/forecast">FORECAST</Link></li>
-                        <li><Link to="/about">ABOUT</Link></li>
-                        <li><Link to="/team">TEAM</Link></li>
-                    </ul>
-                </div>
+        <div className="cover-content">
+            {/* <div className="team-name-container">
+                <h1 className="team-name">Weather Wizards</h1>
+            </div> */}
+            <div className="project-content">
+                <span className='weather-wizard'>Weather Wizard</span>
+                <img src="./src/assets/cloud.svg" alt="cloud" />
             </div>
-        </Fragment>
+            <div className="project-nav">
+                <ul>
+                    <li><Link to="/">HOME</Link></li>
+                    <li><Link to="/forecast">FORECAST</Link></li>
+                    <li><Link to="/about">ABOUT</Link></li>
+                    <li><Link to="/team">TEAM</Link></li>
+                </ul>
+            </div>
+        </div>
     );
 
 }
