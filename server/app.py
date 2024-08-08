43f8f0b40f1dade396d252218f10c914bed00c84
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2024-08-08 02:40:44 -0500

    implemented changes using the new Form class

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from models import ModelCollection
+from form import Form
 
 # To be changed with SQLite db is set up
 LAST_24HR_DATA = []
@@ -12,46 +13,17 @@ CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    form = request.json
-    is_valid = form_validator(form)
-    predictions = []
-    if is_valid:
-        region_name = form.get('region')
-        model_name = form.get('model')
-        model = model_collection.get_model([(region_name, model_name)])
-
+    predictions = dict()
+    form = Form(request.json)
+    if form.is_validated():
+        region_model_tuple = form.to_short_hand()
+        model = model_collection.get_model(region_name=region_model_tuple[0], model_name=region_model_tuple[1])
         # Currently placeholder data until weather api is integrated. Assumes the data is a NumPy array that has already been normalized.
         predictions = model.get_predictions(LAST_24HR_DATA)
         return jsonify({"Predictions": predictions})
     else:
         return jsonify({"Error": "Invalid form data."})
-
-def form_validator(form):
-    """ 
-        Checks that the form keys and values are in the list of possible values
-        @param form: the json form to validate
-
-        Returns the boolean is_valid
-    """
-
-    form_keys = ['region', 'model']
-    form_models = ['lstm']
-    form_regions = ['mongolia']
-
-    is_valid = True
-    for key, value in form.items():
-        if key not in form_keys:
-            valid = False
-            break
-        if key == 'region' and value not in form_regions:
-            valid = False
-            break
-        if key == 'model' and value not in form_models:
-            valid = False
-            break
-
-    return is_valid
-
+    
 if __name__ == "__main__":
     model_collection = ModelCollection()
     model_collection.load_models()
